ab772c0caf99f8f2c8d3c9589f4f3b2e0c40127d
Ramic <ramlag1819@gmail.com>
2023-03-21 14:52:27 -0700

    Fixed and added new route for the individual shortUrlId to redirect to longURL

--- express_server.js
+++ express_server.js
@@ -38,13 +38,17 @@ app.get("/urls/new", (req, res) => {
 app.post("/urls", (req, res) => {
   let id = generateRandomString();
   urlDatabase[id] = req.body.longURL;
-  console.log(req.body);
-  res.send("Ok");
+  res.redirect(`/urls/${id}`);
 })
 
 app.get("/urls/:id", (req, res) => {
   const templateVars = {id: req.params.id, longURL: urlDatabase[req.params.id]};
   res.render("urls_show", templateVars);
+})
+
+app.get("/u/:id", (req, res) => {
+  const longURL = urlDatabase[req.params.id];
+  res.redirect(longURL);
 })
 
 app.get("/urls.json", (req, res) => {
