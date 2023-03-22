3363ada157f23beff4cfc698a5478e6e32d76596
Ramic <ramlag1819@gmail.com>
2023-03-21 21:57:35 -0700

    Modified _header partials to include a username passed in as a cookie through a new POST route

--- express_server.js
+++ express_server.js
@@ -1,6 +1,8 @@
 const express = require('express');
+var cookieParser = require('cookie-parser')
 const app = express();
 const PORT = 8080;
+
 
 const generateRandomString = () => {
   let urlId = "";
@@ -21,6 +23,7 @@ const urlDatabase = {
 app.set("view engine", "ejs");
 
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.send("Hello");
@@ -33,12 +36,13 @@ app.post("/login", (req, res) => {
 })
 
 app.get("/urls", (req, res) => {
-  const templateVars = {urls: urlDatabase};
+  const templateVars = {urls: urlDatabase, username: req.cookies["username"]};
   res.render("urls_index", templateVars);
 })
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = {username: req.cookies["username"]};
+  res.render("urls_new", templateVars);
 })
 
 app.post("/urls", (req, res) => {
@@ -58,7 +62,7 @@ app.post("/urls/:id", (req, res) => {
 })
 
 app.get("/urls/:id", (req, res) => {
-  const templateVars = {id: req.params.id, longURL: urlDatabase[req.params.id]};
+  const templateVars = {id: req.params.id, longURL: urlDatabase[req.params.id], username: req.cookies["username"]};
   res.render("urls_show", templateVars);
 })
 
